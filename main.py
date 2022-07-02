9008720c949efd0f806df1b7ee1560c640e5c860
kylych1988 <kylychsarykov@gmail.com>
2022-07-02 19:07:21 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,704 @@
+# name = "Kurmanbek"
+# print(name[3:6:2])
+
+# names = ('Kurmanbek', 'Adilet','Nurtilek')
+# print(names[1:3])
+
+
+# num1 = int(input("Первое число:"))
+# operation = input("Выберите операцию +,-,*,/:")
+# num2 = int(input("Второе число: "))
+# if operation == "+":
+#     print(num1 + num2)
+# elif operation == "_":
+#     print(num1 - num2) 
+# elif operation == "*":
+#     print(num1 * num2)  
+# elif operation =="/":
+#     print(num1 / num2)
+# import random 
+# random_number = random.randint(1, 5) 
+# print("Случайное число",random_number)
+# user = int(input("Угадайте число: "))
+# if user == random_number:
+#     print("Вы угадали!")
+# else:
+#     print("Вы проиграли")
+#     print("Случаное число было",random_numb
+
+# from importlib.resources import contents
+
+
+# cantacts = ['Kyrmanbek', 'Adilet']
+# command = input("Комманды: 1 -добавить, 2 - удалить, 3 -посмотреть контакты: ")
+# if command == "1":
+#     name = input("Введите имя контакта:")
+#     if name in contacts:
+#         print("такое имя уже есть")
+#     else:
+#         cantacts.append(name)
+#         print("Контакти",name, "успешно добавлен")
+#     print(contacts)
+# elif command == "2":
+#     delete_name =input("Имя контакта которую нужно удалить: ")
+#     if delete_name in contacts:
+#         contacts.remove(delete_name)
+#         print("Контакт", delete_name, "Успешно удален")
+#     else:
+#         print("Такого нету")   
+#     print(contacts) 
+# elif command == "3":
+#     print(contacts)
+# else    
+#     print("Неправильная комманда")
+# from modulefinder import STORE_OPS
+
+
+# str_example_1 ='Hello! my name\'s Kurmanbek'
+# print(str_exam
+ 
+# print(type(True)
+
+# str_example_1 = "Hello! My name\" s Kurmanbek"
+# print(str_example_1)
+# str_example_2 = "Hello! my name's Kurmanbek"
+# print(str_example_2)
+# print(str_example_2)
+# str_example_3 = """Hello! My name's Kurmanbek I'm Python developer"""
+# print(str_example_3)
+
+# print("10" + "10")
+# print (10 - False)
+# print(10 + 10.1)
+
+# text = """сарыков кылычбек шабдан ТОЛОГОН Шабдан Марлен Жакшылык Б . 35 Кылыч. """
+# print(text.upper())
+# print(text.lower())
+
+# name = "KyleHC SarYKOV"
+# print(name.title())
+# a = int(input())
+# if a <0:
+    #  a*-1
+#      print(a)
+# else:
+#     print(a)    
+# 1
+# a = int(input())  
+# b = int(input())  
+# if a > b:
+#     print(a)
+# else:
+#     print:(b) 
+
+# a = int(input())     
+# if a < 0:
+#     print("-")
+# else:
+#     print("+") 
+
+# a = int(input())
+# if a >0:
+#     a = a *-1
+# b = int(input())  
+
+# if a !=b:
+#     print("YES")
+# else:
+    # print(""O")
+# a = int(inp"t())
+# if a == 1 o" a ==12 or a == 2:
+ # print(""има")elif a == 3"or a ==4 or a == 5:
+    # print("весна")   
+#tlif a == or a ==7 or a == 8:
+#     print("лето")  
+# elif a == 9 or a ==10 or a == 11:
+#     print("осень") 
+# else:
+#     print("Введите число от 1 до 12")  
+# a,b,c =
+# map(int,input().split()) 
+# res = o
+# if a > o:
+#     res += 1
+# if b>o:
+#     res +=1
+# if c >o:
+#     res +=1
+# print(res) 
+
+# ge = int (input())
+# mon = int(input()) 
+# res = mon * 29 + (age* 12 *29)  
+# print(res)   a           
+
+# name ="sARYkov KylYCH"
+# print(name.title())
+ 
+# num_1 = int(input("Первое число:")
+# num_2 = int(input("Второе число:")
+# if num_1> num_2
+#         print("Первое число больше второго") 
+# else:
+#         print("Второе число больше первого")      
+# 
+# 
+# НЕЧЕТНЫЕ - 1,3,5,7,9
+# ЧЕТНЫЕ - 2,4,6,8,10  
+
+# num = int(input("Введите число:"))
+# if num % 2 == 0:
+#     print("Четное")
+# else:
+#     print("Нечетное")
+
+# print(10 % 2)
+ 
+# age = int(input())
+# mon = int(input())
+# res = mon * 29 + (age * 12 * 29)
+# print( res )
+
+# a = int(input())
+# if a <0:
+#     a * -1
+#     print(a)
+# else:
+#     print(a)
+
+# it_run = "IT RUN"
+# hello = "Hello world"
+# print(hello + " " + it_run)
+
+
+# print(8 == 10)
+# print(8 == 8)
+# print(30/7)
+
+# print(30 // 7)
+# print(3 ** 3)
+
+# numbers = [1,2,3,4,5,6,7,8]
+# for i in numbers:
+    # if i % 2 == 0:
+        # print(i, "четная")
+    # else:
+        # print(i, "нечетная")    
+
+# for kylych in range(1, 10001):
+#     print(kylych)
+
+# lst = []
+# for n in range(1, 1001):
+#     lst.append(n)
+
+# print(lst)
+
+#цикл while
+# x = 20
+# y = 30
+# while x < y: #цикл будет пока X не достигнет уровня y
+#     x += 1 #C каждым циклом добавлятся 1 
+#     print(x)
+
+#     n = 0
+
+# while True:
+#     # n = n + 1
+#     n += 1
+#     print(n)
+
+
+
+# apps = ["Google Chrome", "Telegram", "What's app","Instagram"]
+# find_app = input("Напишите приложение, которую вы ищите: ") 
+# for i in apps:
+#     if i == find_app:
+#         print("Ваше приложенте найден")
+        # print(i)
+        # break
+
+# numbers = [1,2,3,4,1000,55,99]    
+# for i in numbers:
+    # print(i)
+    # if i == 2:
+        # cont inue
+
+# n = 0
+# while True:
+#     n += 1
+#     print(n)    
+#     if n == 100:
+#         break
+
+# import random    
+
+# from random import random
+
+
+# random_password = random.randint(1,100000)
+# print("Рандомный пароль", random_password)
+# n = 0
+# while True:
+    # n += 1
+    # print(n, random_password)
+    # if n == random_password:
+        # print("пароль найден", n)
+        # break
+
+# a = '00000000'    
+# for i in range(1,6):
+    # print(a,i)
+
+# mumbers =[1,2,3,4,1000,55,99]
+# for i in mumbers:
+#     print(i)
+#     if i == 2:
+#         continue
+# ist = []
+# for n in range (1, 100001):
+#     ist.append(n)
+# mumbers = [1,2,3,4,5,6,7,8,9] (" i,нечетная")
+# from multipro
+# for i in mumbers:
+#     if i % 2 == 0:
+#         print (" i, четная")     
+#     else:
+#         printcessing import parent_process
+
+
+# parent_process ("1  .         1000000")
+# print : i ("in")
+
+# mumbers = [1, 10000]
+# for i in mumbers:
+    # print (1)
+    # print(1000000)  
+
+# for Kylych in range(1, 10001):
+    # print(Kylych)
+
+# lst = []
+# for n in range(1, 1001):
+#     lst.append(n)
+
+# print(lst)
+
+# Ist = []
+# for i in range(1, 1001):
+#     Ist.append(i)
+#     print(Ist)
+# print(min(Ist))   
+# print(max(Ist))
+# print(sum(Ist))
+
+# student = {'name' : 'Adilet', 'age' : 17}
+# print(student['age'])
+# student['course'] = 'pyton'
+# print(student)
+# del student['course']
+# print(student)
+# student.setdefault('course', 'javascript')
+# print(student)
+# student.pop('name')
+# print(student)
+# name = "IT-Run"
+# it_run = {'name' : 'IT-RUN', 'adress':'Masalieva 38', 'experience':'2 years'}
+# print(it_run.keys())
+
+# print(it_run.values())
+# print(it_run.items())
+
+# for key, value in it_run.items():
+    # if key == 'adress'and value == 'Masalieva 38':
+#         # print(key, value)
+
+# from turtle import update
+
+
+# contacts = {'Kurmanbek' : '0777876543', 'Adilet': '0556764543'}
+# while True:
+#     command = input("Введите команду, 1 - добавит в контакты, 2 - Удалить из контакта, 3 - обновить номер, 4- посмотреть все контакты: ")
+#     if command == "1":
+#         add_name = input("Введите имя которую нужно добавить: ")    
+#         add_number = input("Введите номер:")
+#         if add_name in contacts.keys():
+#             print("Такое имя есть")
+#         else:
+#             contacts.setdefault(add_name, add_number)    
+#             print("Контакт", add_name, "Успешна добавлен")
+#     elif command == "2" :
+#         delete_name = input("Введите имя которую нужно удалить:")       
+#         if delete_name in contacts.keys():
+#             contacts.pop(delete_name)
+#             print("Контакт", delete_name, "Успешно удалит")
+#         else: 
+#             print("Такого нету")
+#     elif command == "3":
+#         update_name = input("Введите имя: ")
+#         if update_name in contacts.keys():
+#             update_number = input("Введите новый номер: ")
+#             contacts[update_name] = update_number
+#             print("Номер для", update_name,"Успешно обновил")
+#         else:
+#             print ("Такого нету") 
+#     elif command == "4":
+#         for name, number in contacts.items():
+#             print(name, number)
+#     elif command == "break":
+#         print("Программа отключена")
+#         break
+#     else:
+#         print("Такой комманды н 
+
+
+# from tokenize import Name
+
+# student = {'name' : 'Adilet'}
+# print(type (student))
+
+# student = {'name' : 'Adilet', 'age' : 17}
+# # print(student[ 'age'])
+# student ['age'] = 18
+# student['course'] = 'python'
+# print(student)
+# del student['course']
+# print(student)
+# student.setdefault('course', 'javascript')
+# print(student)
+# student.pop('name')
+# # print(student)
+# for key, value in it_run.items():
+#     if key == 'adress' and value == 'Masalieva 38' :
+#         print(key, value)
+# student = {'name' : 'kylych', 'age' : '20', 'eliza': 'raiana', 'ymar' : 'jakhclyk',} 
+# print(student[ 'ymar'])
+# student ['name'] = kylych
+# student['course'] = 'python'
+# print(student)
+# del student['course']
+# print(student)
+# student.setdefault('course','javascript')
+# print(student)
+# student.pop('name')
+# print(student
+
+# umbers = [1,2,3,4,5,6,7,8,9] (" i,нечетная")
+# fro  multipro
+# for i in mumbers:
+#     if i % 2 == 0:
+#         print (" i, четная")     
+#     else:
+#         printcessing import parent_process
+
+# for kylych in range 1, (10001):
+    # print (kylych)
+
+ 
+# a = [1,2,3,4,5]
+# b = [3,4,5,6,7]
+# c = a + b
+# print(set(c))   
+
+# from sys import stdout
+
+
+# stdout = {"Kylychbek", "Tologon", "Aiperi", "marlen", "Apam", "Atam"}
+# print(stdout)
+
+
+
+
+# student = {"Nurbolot", "Aibala","Kylychbek", "Baiastan","Nurbolot", "Aibala"}
+# print(student)
+
+
+# cars = {"Audi", "BMW", "Mersedes", "Lexus", "Lexus"}
+# print(cars)
+# cars.add("Honda")
+# print(cars)
+# cars.remove("BMW")
+# print.pop()
+# print(cars)
+
+# mumbers = {1,2,3,4,5,6,7}
+# for i in mumbers:
+#     print(i)
+
+# numbers = {100,1,2,3,4,1000,2,3,50,}
+# print(sorted(numbers))
+# hello = {1,1.0, True}
+# print(hello)
+
+# import numbers
+
+
+# numbers = {100,1,2,3,4,1000,2,3,50}
+# frzn = frozenset(numbers)
+
+# print(frzn)
+ 
+
+# try:
+#     print(100/0)
+# except ZeroDivisionError:
+#     print("ноль делить нельзя! Учи математику!")
+
+# while True:
+#     num_1 = int(input("Певое число:"))
+#     num_2 = int(input("Второе число:"))
+#     try:
+#         print(num_1 / num_2)
+#     except:
+
+#         print("Ошибка")
+#     finally:
+#         print("В люьбом случи сработаю")
+
+# num_1 = int(input("Первое число:"))
+# num_2 = int(input("Второе число:"))
+# if num_1 == 0 and num_2 == 0:
+#     raise NameError("Вы не можете взломать")
+# else:
+#     print(num_1 + num_2)
+    
+
+# from re import A
+# from unicodedata import name
+
+
+# nambers: ("Kylychbek"(umar)
+# try except
+# print: nambers
+
+# nambers:(1-Ввод: 10:00:30)
+#     ('try, except')
+# (print:namber
+
+# ZeroDivisionError  (10000/0)
+# try:
+#     print(10000/0)
+# except ZeroDivisionError:
+#     print("zerodisione
+
+
+# while True:
+#     h1, m1, s1 = input("Введите время 1 input! Пример: 00:00:00 ").split(':')
+#     h1,m1, s1 = int(h1), int(m1), int(s1)
+
+#     h2, m2, s2 = input("Введите время! пример: 00:00:00 ").split(':')
+#     h2, m2, s2 = int(h2), int(m2), int(s2)
+
+#     if h1>=24 or m1 >=60 or s1>=60:
+#         print("Не удалось выяснить! . пишите виремя правильно !!!")                                                                            
+#     elif h2>24 or m2>=60 or s2>=60:
+#         print("Не удалось выяснить!. Пишите время правильно !!!")
+#     else:
+#         res1 = h1*3600 + m1 *60 + s1
+#         res2 = h2*3600 + m2 *60 + s2
+#         print(res2-res1)
+# Выведите на экран все четные в диапазоне от 1 до 400 и запишите в пустой список
+
+# Ist = []
+# for i in range(0, 401, 2):
+#     Ist.append(i)
+# print(Ist)
+
+
+# x = int(input("пожалуйств введите число:\n"))
+
+# int_type = 'четным' if x % 2 == 0 else 'не четным'
+
+# print(f'Введеный вами {x} являеться {int_type} числом.')
+
+# number = 5 if 6 == 5 else 6
+# print(number)
+
+# from ast import ListComp
+
+
+# Number_12 = 12
+# y = "sarykovrylych"
+# tr = True
+# list = [12,324,453,4353]
+# print (f'Введеный вам {Number_12} являеться {y} {tr} {list} числом.')
+
+# nums =[n for n in range(101) if n%2 == 1]
+# for n in range(101):
+#     nums.append(n)
+
+# print(nums)
+
+# mums = ['p', 'y', 't', 'h','o', 'n']
+# print(mums)
+
+# list = [i for i in "kylych"]
+# print(list)
+
+# from re import I
+
+
+# Iist = [i for i in "jakshylyk"]
+# print (I)
+
+# import numbers
+
+
+# numbers = (15,22,4,6,8,248,500,785)
+# print(sorted(numbers))
+# from operator import rshift
+
+# from pkg_resources import run_script
+
+# from ast import AugAssign
+
+
+# Есть ,словарь ,adilet = {,name, : ‘Adilet’, ‘age’ : 17, ‘color’ : ‘White’}. Умножьте его age на 2 раза 
+# print= i in import(adilet : AugAssign
+
+
+
+
+# a = 100
+# b = 10
+# if a > b :
+#     print(a)
+# else:
+#     print(b)
+
+
+# res = a if a > b else b
+# print(res)
+
+# print (a if a > b else b)
+
+# a = 10
+# b = 2000
+# c = 3000
+# if a> b and a > c:
+#     print(a)
+# elif b > a and b > c:
+#     print(b)
+# elif c > a and c > b:
+#     print(c)
+
+# a = 10
+# b = 2000
+# c = 3000
+# res = a if a > b and a > c else b if b > a and b > c else c
+# print(res)
+
+# nums = []
+# for i in range(1, 101):
+#     nums.append(i)
+# print(nums)
+
+# nums = [i for zi in range(1, 101)]
+# print(nums)
+
+# chet = []
+# for i in range(1,51):
+#     if i % 2 == 0:
+#         chet.append(i)
+# print(chet)
+
+# chet = [i for i in range(1, 51,) if i % 2 == 0]
+# prin/t(chet)
+
+# petrol = {
+#     'Ai 100' : 60,
+#     'Ai 95' : 55,
+#     'Ai 92' : 50,
+#     'Ai 80' : 35,
+#     'DT' : 30,
+# }
+# petrol['Ai 95'] = 58
+# print(petrol)
+
+# ne/
+
+# new_petrol = {k:v+ 10 for k, v in petrol.items()}
+# print(new_petrol)
+
+
+# sort_petrol = {}
+# for k, v in new_petrol.items():
+#     sort_petrol.setdefault(k + "A", v)
+#     print(sort_petrol)
+
+# sort_petrol = {k + " A":v for k, v in new_petrol.items()}
+# print(sort_petrol)
+
+# from tokenize import Number
+
+
+# def kylych():
+#     print("Hello kylych")
+# # kylych()    
+# def add():
+#     num1 = int (input("Первое число:"))
+#     num2 = int (input("Второе число:"))
+#     print(num1 + num2)
+# # add()
+# def chet():
+    # Number = int (input("Введите число:"))
+    # if Number % 2 == 0:
+#         print("Четный")
+# # chet():        
+#     else:
+#         print("Не чет")
+# chet():
+
+# def parametr(name): 
+#     print("Добро пожаловать",name)
+
+# parametr("Nurbolot")
+# parametr("Adilet")
+# parametr("Aibala")
+# parametr("kylychbek")
+
+# name = input("yuor name: ")
+# print("Добрый пожалват", name)
+
+# num_1 = float (input("Num1:"))
+# num_2 = float (input("Num2:"))
+# print (num_1 + num_2)
+
+# name = "kylychbek"
+       #012345678
+# print(name[3])
+# print(name[2:9])
+
+# name = "umarbek"
+       #0123456
+# print(name[2])
+# print(name[1:6]
+# import numbers
+
+
+
+
+# numbers = [10,15,12,1,2,4,6,100,1000,]
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
+
+
+# def calc(num1, num2, operation):
+#     if operation == "+":
+#         print(num1 + num2)
+#     elif operation == "-":
+#         print(num1 - num2)
+#     elif operation == "*":
+#         print(num1 * num2)
+#     elif operation == '/':
+#         try:
+#             print(num1 / num2)
+#         except:
+#             print("Нольго болсо болбойт")
+
+# calc(10, 20, "+") #Аргументы
+# calc(100, 20, "-")
+# calc(20, 20, "+")
+# calc(20, 20, "*")
+# calc(20, 0, "*")
